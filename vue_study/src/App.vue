8de159a910f308f26c1c77968519ebfc653a89ae
xtm <2083933996@qq.com>
2025-09-12 20:36:54 +0800

    feat

--- vue_study/src/App.vue
+++ vue_study/src/App.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<div v-if="show">显示</div>
+<button @click="show = !show">按钮</button>
+<div v-for="(item, index) in list" :key = "item">
+    {{ index }}{{ item }}
+
+  <div><input placeholder="请输入内容" @keydown.enter= "add"></div>
+</div>
+
+
 </template>
 
+
+
+<script setup lang="ts">
+import {reactive,ref} from "vue";
+let show = ref(true)
+const list = ref(["小宋","小雪","小二"])
+function add (e) {
+  console.log(e)
+}
+</script>
 <style scoped></style>
