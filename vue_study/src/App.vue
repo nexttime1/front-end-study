acee730a9b4e5d411eab317586b0ac3e60e13cc4
xtm <2083933996@qq.com>
2025-09-13 20:53:25 +0800

    feat-9/13

--- vue_study/src/App.vue
+++ vue_study/src/App.vue
@@ -1,23 +1,98 @@
 <template>
-<div v-if="show">显示</div>
-<button @click="show = !show">按钮</button>
-<div v-for="(item, index) in list" :key = "item">
-    {{ index }}{{ item }}
+  <div v-if="show">显示</div>
+  <button @click="show = !show">按钮</button>
 
-  <div><input placeholder="请输入内容" @keydown.enter= "add"></div>
-</div>
+  <div v-for="(index, item) in list" :key="item">
+    {{ item }} {{ index }}
+  </div>
 
-
+  <div>
+    <input placeholder="请输入内容" @keydown.enter="add">
+  </div>
+  <!-- 使用注册的组件 -->
+  <numberClick :num = "3" @plus = "plus"></numberClick>
 </template>
 
+<script>
+import { ref } from "vue";
+import numberClick from "@/components/numberClick.vue";
+export default {
+  // 注册组件
+  components: {
+    numberClick
+  },
+  // 数据
+  data() {
+    return {
+      message: "初始消息"
+    }
+  },
 
+  // 方法
+  methods: {
+    updateMessage() {
+      this.message = "更新后的消息";
+    },
+    plus(num, msg){
+  console.log(num, msg)
+} 
+  },
+  
+  setup() {
+    let show = ref(true);
+    const list = ref(["小宋", "小雪", "小二"]);
+    
+    function add(e) {
+      // 添加输入内容到列表
+      if (e.target.value.trim()) {
+        list.value.push(e.target.value.trim());
+        e.target.value = ""; // 清空输入框
+      }
+    }
+    
+    // 必须返回才能在模板中使用
+    return {
+      show,
+      list,
+      add
+    }
+  },
 
-<script setup lang="ts">
-import {reactive,ref} from "vue";
-let show = ref(true)
-const list = ref(["小宋","小雪","小二"])
-function add (e) {
-  console.log(e)
-}
+  // 生命周期钩子
+  beforeCreate() {
+    console.log("1. beforeCreate：实例刚创建，data和methods还未初始化");
+    console.log("此时访问data：", this.message);
+  },
+
+  created() {
+    console.log("2. created：实例创建完成，data和methods已初始化");
+    console.log("此时访问data：", this.message);
+  },
+
+  beforeMount() {
+    console.log("3. beforeMount：模板编译完成，等待挂载到DOM");
+  },
+
+  mounted() {
+    console.log("4. mounted：组件已挂载到DOM");
+  },
+
+  beforeUpdate() {
+    console.log("5. beforeUpdate：数据已更新，DOM尚未更新");
+  },
+
+  updated() {
+    console.log("6. updated：数据更新且DOM已重新渲染");
+  },
+
+  beforeUnmount() {
+    console.log("7. beforeUnmount：组件即将卸载");
+  },
+
+  unmounted() {
+    console.log("8. unmounted：组件已完全卸载");
+  }
+};
 </script>
+
 <style scoped></style>
---
+++ vue_study/src/components/numberClick.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="btn">
+    <button @click="add">++</button>
+    <span>当前值：{{ number }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+
+interface Props {
+  num ?: number    // ? 可选参数
+}
+//接参数   设置默认值
+const props = withDefaults(defineProps<Props>(), {
+  num : 1,
+})
+//点击之后通知 父
+const emits = defineEmits<Emits>() 
+
+interface Emits {
+(e:"plus", num : number , msg : string): void
+
+}
+
+
+
+const number = ref(0)
+
+function add() {
+ number.value += props.num
+ emits("plus", number.value, "我被点击了")
+}
+</script>
+
+<style scoped>
+.btn {
+  display: flex;
+  align-items: center;
+}
+</style>
