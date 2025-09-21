8f4262e7d53fe9233e2387b324ab845827cb3a6c
xtm <2083933996@qq.com>
2025-09-21 17:08:30 +0800

    feat-9/21

---
+++ axios_study/axios_learn/src/上传文件.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="file" name="file" id="upload" @change="upload">
+  </div>
+</template>
+
+<script setup lang="ts">
+import axios from "axios";
+
+function upload(e: Event) {
+
+  const target = e.target;
+  if (target instanceof HTMLInputElement) {
+    const files = target.files;
+    if (files) {
+      const formData = new FormData()
+      for (let i = 0; i < files.length; i++) {
+        formData.append('file', files[i])
+      }
+      axios.post("http://localhost:8080/upload", formData, {headers: {token: "xxx"}}).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
